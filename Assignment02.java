907879659537a55428c5ef2446bca0a7ed0c7f19
hanstoj <stojack.hannah@gmail.com>
2021-01-26 13:09:34 -0800

    ready for logic

---
+++ Assignment02.java
@@ -0,0 +1,37 @@
+// CSE 110     : CSE 110
+// Assignment  : <assignment #02
+// Author      : Hannah Stojack & <studentID>
+// Description : <of the file contents>
+
+import java.util.Scanner;
+
+public class Assignment02 {
+
+    public static void main(String[] args) {
+        // declare and instantiate a Scanner
+        Scanner input = new Scanner(System.in);
+        // declare and initialize variables
+        double roadLength;
+        int numLanes;
+        int depthAsphalt;
+        int projectDays;
+
+        // collect inputs
+        System.out.println("What is the desired road length? ");
+        roadLength = input.nextDouble();
+
+        System.out.println("How many lanes? ");
+        numLanes = input.nextInt();
+
+        System.out.println("How many inches deep will the asphalt be? ");
+        depthAsphalt = input.nextInt();
+
+        System.out.println("How many days will the project take? ");
+        projectDays = input.nextInt();
+        // compute required values
+
+        // display results
+
+    }
+
+}
